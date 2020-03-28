b2a657b1c85268eb8a3d0076e4cb0f4f4e8a553a
Saad Ismail <saad3112@gmail.com>
2020-03-28 18:16:35 +0500

    add Rover and RoverTest

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,7 +4,6 @@ public class MainTest {
 
     @Test
     public void test() {
-        assert(true);
-
+        
     }
 }
---
+++ src/test/java/RoverTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class RoverTest {
+
+    @Test
+    public void roverTest() {
+        Rover rover = new Rover();
+    }
+
+}
