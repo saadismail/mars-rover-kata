dbefff4060137d77a0f9995d85c8eb08b5f91e13
lamak <lamak.qaizar@careem.com>
2020-03-28 19:25:57 +0500

    Added EAST direction

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,4 +1,4 @@
 public enum Direction {
 
-    NORTH, SOUTH;
+    NORTH, SOUTH, EAST;
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -5,20 +5,36 @@ public class RoverTest {
 
     @Test
     public void roverInitializationTest_ORIGIN() {
-        Direction direction = Direction.NORTH;
         Coordinate initialCoordinate = Coordinate.ORIGIN;
-        Rover rover = new Rover(initialCoordinate, direction);
+        Rover rover = new Rover(initialCoordinate, Direction.NORTH);
         Assert.assertEquals(initialCoordinate, rover.getCoordinate());
+    }
+
+    @Test
+    public void roverInitializationTest_NON_ORIGIN() {
+        Coordinate initialCoordinate = new Coordinate(123, 456);
+        Rover rover = new Rover(initialCoordinate, Direction.NORTH);
+        Assert.assertEquals(initialCoordinate, rover.getCoordinate());
+    }
+
+    @Test
+    public void roverInitializationTest_NORTH() {
+        Direction direction = Direction.NORTH;
+        Rover rover = new Rover(Coordinate.ORIGIN, direction);
         Assert.assertEquals(direction, rover.getDirection());
     }
 
     @Test
-    public void roverInitializationTest_NONORIGIN() {
+    public void roverInitializationTest_SOUTH() {
         Direction direction = Direction.SOUTH;
-        Coordinate initialCoordinate = new Coordinate(123, 456);
-        Rover rover = new Rover(initialCoordinate, direction);
-        Assert.assertEquals(initialCoordinate, rover.getCoordinate());
+        Rover rover = new Rover(Coordinate.ORIGIN, direction);
         Assert.assertEquals(direction, rover.getDirection());
     }
 
+    @Test
+    public void roverInitializationTest_EAST() {
+        Direction direction = Direction.EAST;
+        Rover rover = new Rover(Coordinate.ORIGIN, direction);
+        Assert.assertEquals(direction, rover.getDirection());
+    }
 }
