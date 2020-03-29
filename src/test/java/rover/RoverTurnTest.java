c235bde94eff57431639372b68217befa17cdf22
lamak <lamak.qaizar@careem.com>
2020-03-29 20:00:23 +0500

    Refactored turnRight test to be parameterized

--- src/test/java/rover/RoverTurnTest.java
+++ src/test/java/rover/RoverTurnTest.java
@@ -2,20 +2,41 @@ package rover;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static rover.Coordinate.ORIGIN;
+import static rover.Direction.EAST;
+import static rover.Direction.NORTH;
+import static rover.Direction.SOUTH;
+import static rover.Direction.WEST;
+
+@RunWith(Parameterized.class)
 public class RoverTurnTest {
 
-    @Test
-    public void turnRightWhenFacingNorth() {
-        Rover rover = new Rover(Coordinate.ORIGIN, Direction.NORTH);
-        rover.turnRight();
-        Assert.assertEquals(Direction.EAST, rover.getDirection());
+    @Parameter
+    public Direction initial;
+
+    @Parameter(1)
+    public Direction expected;
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {NORTH, EAST},
+                {EAST, SOUTH}
+        });
     }
 
     @Test
-    public void turnRightWhenFacingEast() {
-        Rover rover = new Rover(Coordinate.ORIGIN, Direction.EAST);
+    public void turnRightTest() {
+        Rover rover = new Rover(ORIGIN, initial);
         rover.turnRight();
-        Assert.assertEquals(Direction.SOUTH, rover.getDirection());
+        Assert.assertEquals(expected, rover.getDirection());
     }
 }
